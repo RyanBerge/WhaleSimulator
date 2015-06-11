1048d8f3b25116d6edadb4743f9f23e830d939ad
RothranAgnos <rothran.agnos1218@gmail.com>
2015-06-11 14:12:26 -0700

    dr

--- WhaleSimulator/WhaleSimulator/WhaleSimulator/SkyBox.cs
+++ WhaleSimulator/WhaleSimulator/WhaleSimulator/SkyBox.cs
@@ -8,18 +8,16 @@ using Microsoft.Xna.Framework.Content;
 
 namespace WhaleSimulator
 {
+    // class that implements a skybox into the program and renders through a custom shader effect
     public class Skybox
     {
-        public Model skyBox;
+        public Model skyBox; // the model of the skybox
 
-        private Texture2D skyBoxTexture;
+        private Texture2D skyBoxTexture; // the skybox texture
 
-        // private Texture2D underWaterTexture;
+        private Effect skyBoxEffect; // the effect that hold information from the shader file
 
-        private Effect skyBoxEffect;
-
-        // private float size = 1000f;
-
+        // loads the model, texture, and hlsl files for the skybox
         public Skybox(ContentManager Content)
         {
             skyBox = Content.Load<Model>("Effects/cube");
@@ -27,6 +25,7 @@ namespace WhaleSimulator
             skyBoxEffect = Content.Load<Effect>("Effects/Skybox");
         }
 
+        // draws the skybox using the skyboxEffect shader
         public void Draw()
         {   
             foreach (EffectPass pass in skyBoxEffect.CurrentTechnique.Passes)
@@ -37,6 +36,7 @@ namespace WhaleSimulator
                 {
                     foreach (ModelMeshPart part in mesh.MeshParts)
                     {
+                        // passes in the variables in the program down to the shader pipeline
                         part.Effect = skyBoxEffect;
                         part.Effect.Parameters["World"].SetValue(
                             Matrix.CreateScale(Camera.FarClippingPlane*0.9f) * Matrix.CreateTranslation(Camera.Position.X, 950, Camera.Position.Z));
